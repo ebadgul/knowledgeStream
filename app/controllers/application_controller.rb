c2dc3e044bd5f314bebc08fe5ab728c7604cbf9b
Ebadullah Gul <ebadgul66@gmail.com>
2015-11-22 17:51:52 +0000

    show flash messages to user for authenticate and redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  include SessionsHelper
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+    include SessionsHelper
 
-  def authenticate_user
-      redirect_to login_path unless logged_in?
-  end
+    def authenticate_user
+        unless logged_in?
+            flash[:error] = "You need to log in."
+            redirect_to login_path
+        end
+    end
 
-  def redirect_logged_in_user
-      redirect_to user_profile_path(current_user) if logged_in?
-  end
+    def redirect_logged_in_user
+        if logged_in?
+            flash[:error] = "You are already logged in."
+            redirect_to user_profile_path(current_user)
+        end
+    end
 end
