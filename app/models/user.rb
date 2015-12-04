e85887148f080432d5feb238a24a482e138f3b81
Ebadullah Gul <ebadgul66@gmail.com>
2015-12-04 20:59:09 +0000

    added comments model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
     has_many :posts
+    has_many :comments
 
 
     # Returns the hash digest of a string.
