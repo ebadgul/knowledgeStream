09e6e642a3ea51d702176a6b764e6d2a5fac882a
Ebadullah Gul <ebadgul66@gmail.com>
2015-12-07 01:42:57 +0000

    improved comment history look

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_histories
 
-  before_save :update_history
 
+  before_save :update_history
   def update_history
-      unless self.new_record?
-          post = Post.find(self.id)
-          post_history = PostHistory.new
-          post_history.post_id = self.id
-          post_history.title = post.title
-          post_history.description = post.description
-          post_history.save
-      end
+    unless self.new_record?
+      post = Post.find(self.id)
+      post_history = PostHistory.new
+    post_history.post_id = self.id
+    post_history.title = post.title
+    post_history.description = post.description
+    post_history.save
+    end
   end
 end
